a813b5b36bfe354a02e18ce726eedd9c9dd9358c
Stephanny Chaves <stefannychaves1001@gmail.com>
2023-02-20 14:08:47 -0300

    first

---
+++ src/script/index.js
@@ -0,0 +1,43 @@
+ const imagensPainel = document.querySelectorAll('.imagem-painel');
+ const setaAvancar = document.getElementById('btn-avancar');
+ const setaVoltar = document.getElementById('btn-voltar');
+let imagemAtual = 0;
+
+
+function esconderImagens(){
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar')
+        });
+}
+
+setaAvancar.addEventListener('click', function(){
+   const totalDeImagens = imagensPainel.length -1;
+   if(imagemAtual===totalDeImagens){
+    return;
+   }else{
+
+   }
+   
+    imagemAtual++;
+    esconderImagens()
+
+    imagensPainel[imagemAtual].classList.add('mostrar');
+
+
+});
+
+
+setaVoltar.addEventListener('click', function(){
+    if(imagemAtual===0){
+     return;
+    }else{
+ 
+    }
+    
+     imagemAtual--;
+     esconderImagens()
+ 
+     imagensPainel[imagemAtual].classList.add('mostrar');
+ 
+ 
+ });
